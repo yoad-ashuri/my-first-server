0941fbc8bd57fc8674d5f631fcd87932f7719090
yoad-ashuri <68894486+yoad-ashuri@users.noreply.github.com>
2020-10-22 19:32:47 +0300

    and more route to the server

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/", function(req, res) {
   res.send("<h1>Hello World</h1>");
 });
 
+app.get("/about", function(req, res) {
+  res.send("My name is Yoad Ashuri and I love sports and code");
+});
+
 app.listen(3000, function(){
   console.log("Server started at port 3000");
 });
